31a12c72c6885044d156cc80a91f9800abb0f96e
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 18:49:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,7 +13,7 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			if (text = 'hi') {
+			if ($event['message']['text'] = 'hi') {
 			// Get text sent
 				$text = $event['message']['text'];
 			// Get replyToken
