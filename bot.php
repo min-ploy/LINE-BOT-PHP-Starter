e3bc84137ad562a7e0e51ffb4173354a2cf9c830
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 17:59:12 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'หวัดดี' => $text = 'ดีจ้าาา'
+				'text' => $text = 'ดีจ้าาา'
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
