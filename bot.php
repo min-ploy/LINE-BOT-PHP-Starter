a9c52fc8f6d694550a325f7abfcb738c21f97c31
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 18:38:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -46,9 +46,9 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text' && $event['message']['text'] == 'ploy') {
 			// Get text sent
-			$text = 'ploy';
+			$text = $event['message']['text'];;
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
