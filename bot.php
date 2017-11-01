ae22933cd2405f69cc0d79a77f33689f0e084511
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 19:24:58 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -32,7 +32,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'hi' => [$messages]
+				'messages' => [$messages]
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
