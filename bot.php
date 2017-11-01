97a121f7d963008bc1028b6ec23ab44f8f6f0688
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 18:33:09 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -19,18 +19,11 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			if ($text = 'hi'){
-				$messages = [
-					'type' => 'text',
-					'text' => $text = 'ดีจ้าาา'
-				];
-			}
-			if ($text = 'ploy'){
-				$messages = [
-					'type' => 'text',
-					'text' => $text = 'ploy'
-				];
-			}
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
