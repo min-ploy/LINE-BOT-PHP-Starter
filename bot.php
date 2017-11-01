0e446c943d6247343f67a3e6a283c86e93bdd7c7
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-01 18:47:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -13,16 +13,18 @@ if (!is_null($events['events'])) {
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			if (text = 'hi') {
 			// Get text sent
-			$text = $event['message']['text'];
+				$text = $event['message']['text'];
 			// Get replyToken
-			$replyToken = $event['replyToken'];
+				$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => 'hi'
-			];
+				$messages = [
+					'type' => 'text',
+					'text' => 'hi'
+				];
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
