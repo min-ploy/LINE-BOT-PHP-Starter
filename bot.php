796a65adf068bc337e1cf358f16a69632fa43a04
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-03 10:35:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -31,21 +31,7 @@ if (!is_null($events['events'])) {
 						'replyToken' => $replyToken,
 						'messages' => [$messages]
 					];
-					$post = json_encode($data);
-					$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-					$ch = curl_init($url);
-					curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-					curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-					curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-					curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-					curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			
-			
-					$result = curl_exec($ch);
-					curl_close($ch);
-
-					echo $result . "\r\n";
+					
 					break;
 					
 				case 'สบายดีมั้ย' :
@@ -408,6 +394,21 @@ if (!is_null($events['events'])) {
 					echo $result . "\r\n";
 					break;					
 			}
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
+			
+			$result = curl_exec($ch);
+			curl_close($ch);
+
+			echo $result . "\r\n";
 		}
 	}
 }
