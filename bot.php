800b63172f3ebccd11ab4ba231ead9c6c07a6c5d
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-08 15:41:13 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -243,6 +243,8 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages]
 			];
 		}
+		$post = json_encode($data);
+		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 		$ch = curl_init($url);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
