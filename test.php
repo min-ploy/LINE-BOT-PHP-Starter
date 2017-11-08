f63285d204d1a8e98a50a1b1b6b07ddb9cf03564
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-08 16:06:59 +0700

    Update test.php

--- test.php
+++ test.php
@@ -4,10 +4,13 @@ require_once '../vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\FirePHPHandler;
+
 $logger = new Logger('LineBot');
 $logger->pushHandler(new StreamHandler('php://stderr', Logger::DEBUG));
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV["LINEBOT_ACCESS_TOKEN"]);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV["LINEBOT_CHANNEL_SECRET"]]);
+
 $signature = $_SERVER['HTTP_' . \LINE\LINEBot\Constant\HTTPHeader::LINE_SIGNATURE];
 try {
 	$events = $bot->parseEventRequest(file_get_contents('php://input'), $signature);
@@ -20,6 +23,8 @@ try {
 } catch(\LINE\LINEBot\Exception\InvalidEventRequestException $e) {
 	error_log('parseEventRequest failed. InvalidEventRequestException => '.var_export($e, true));
 }
+
+
 foreach ($events as $event) {
 	// Postback Event
 	if (($event instanceof \LINE\LINEBot\Event\PostbackEvent)) {
