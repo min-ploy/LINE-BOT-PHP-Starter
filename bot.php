48160f73279fde1b27050eac7a0cd795b58f5615
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-08 19:43:48 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -398,33 +398,8 @@ if (!is_null($events['events'])) {
 									
 				default :
 					$messages = [
-						"type" => "template",
-						"altText" => "this is a carousel template",
-  						"type" => "carousel",
-						"columns" = [
-							{
-								"thumbnailImageUrl" => "https://example.com/bot/images/item1.jpg",
-								"title" => "this is menu",
-								"text" => "description",
-								"actions" = [
-									{
-										"type" => "postback",
-										"label" => "Buy",
-										"data" => "action=buy&itemid=111"
-									},
-									{
-										"type" => "postback",
-										"label" => "Add to cart",
-										"data" => "action=add&itemid=111"
-									},
-									{
-										"type" => "uri",
-										"label" => "View detail",
-										"uri" => "http://example.com/page/111"
-									}
-								]
-							}
-						]
+						'type' => 'text'
+						'text' => 'กรุณาสอนบอท'
 					];
 					// Make a POST Request to Messaging API to reply to sender
 					$url = 'https://api.line.me/v2/bot/message/reply';
