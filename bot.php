dbb0ea37ec4d456fc538806608ebb72e769f7e27
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-08 15:30:17 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -91,6 +91,18 @@ if (!is_null($events['events'])) {
 						'replyToken' => $replyToken,
 						'messages' => [$messages, $mess]
 					];
+					$columns = array();
+					$img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+					for($i=0;$i<5;$i++) {
+  						$actions = array(
+							new \LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder("Add to Cart","action=carousel&button=".$i),
+							new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("View","http://www.google.com")
+						);
+  						$column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Title", "description", $img_url , $actions);
+						$columns[] = $column;
+					}
+					$carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
+					$outputText = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
 					break;
 				
 				case 'น่ารัก' :
