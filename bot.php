58146498375c76542a2e80dda04662e4ecb47f02
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-08 19:41:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -398,29 +398,29 @@ if (!is_null($events['events'])) {
 									
 				default :
 					$messages = [
-						"type": "template",
-						"altText": "this is a carousel template",
-  						"type": "carousel",
-						"columns": [
+						"type" => "template",
+						"altText" => "this is a carousel template",
+  						"type" => "carousel",
+						"columns" = [
 							{
-								"thumbnailImageUrl": "https://example.com/bot/images/item1.jpg",
-								"title": "this is menu",
-								"text": "description",
-								"actions": [
+								"thumbnailImageUrl" => "https://example.com/bot/images/item1.jpg",
+								"title" => "this is menu",
+								"text" => "description",
+								"actions" = [
 									{
-										"type": "postback",
-										"label": "Buy",
-										"data": "action=buy&itemid=111"
+										"type" => "postback",
+										"label" => "Buy",
+										"data" => "action=buy&itemid=111"
 									},
 									{
-										"type": "postback",
-										"label": "Add to cart",
-										"data": "action=add&itemid=111"
+										"type" => "postback",
+										"label" => "Add to cart",
+										"data" => "action=add&itemid=111"
 									},
 									{
-										"type": "uri",
-										"label": "View detail",
-										"uri": "http://example.com/page/111"
+										"type" => "uri",
+										"label" => "View detail",
+										"uri" => "http://example.com/page/111"
 									}
 								]
 							}
