130c90ff02b9242678cae18b1c7c2c37d4df92db
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-09 23:32:54 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -59,7 +59,7 @@ if ($message->{"text"} == 'confirm') {
     ];
     
 } elseif ($message->{"text"} == 'carousel') {
-    $img_url = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
+    $img = "https://cdn.shopify.com/s/files/1/0379/7669/products/sampleset2_1024x1024.JPG?v=1458740363";
     $messageData = [
         'type' => 'template',
         'altText' => 'carousel',
