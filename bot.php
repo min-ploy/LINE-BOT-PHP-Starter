584e72d3b6232103e5c4b8859b0baf56dcd82d38
min-ploy <32128262+min-ploy@users.noreply.github.com>
2017-11-10 21:47:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,7 +45,7 @@ if ($message->{"text"} == 'ร้านอาหาร') {
 			'columns' => [
 				[
 					'title' => "$name[0]",
-					'text' => 'ที่อยู่',
+					'text' => "$address[0]",
 					'actions' => [
 						[
 							'type' => 'postback',
